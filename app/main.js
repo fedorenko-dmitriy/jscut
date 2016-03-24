1c6243f6843bd2d1f974281e0647303c56855eb9
Dmytro Fedorenko <you@example.com>
2016-03-24 17:59:15 +0200

    taskView#2

--- app/main.js
+++ app/main.js
@@ -3,9 +3,34 @@ var $ = require('jquery-untouched');
 var Backbone = require('backbone');
 Backbone.$ = $;
 
-import { taskView } from './views/taskView';
+import { TaskView } from './views/taskView';
+
+let taskData = [
+  {
+    number: 1,
+    taskName : "Some name#1",
+    taskDescription: "Some description",
+    taskSolution: ""
+  },
+  {
+    number: 2,
+    taskName : "Some name#2",
+    taskDescription: "Some description",
+    taskSolution: ""
+  },
+  {
+    number: 3,
+    taskName : "Some name#3",
+    taskDescription: "Some description",
+    taskSolution: ""
+  }
+];
+
+for(let i = 0; i<taskData.length; i++){
+  let taskView = new TaskView({taskData: taskData[i]});
+  $('body').append(taskView.render().$el);
+}
 
 
-$('body').append(taskView.render().$el);
 
 
--- app/views/taskView.js
+++ app/views/taskView.js
@@ -5,24 +5,14 @@ Backbone.$ = $;
 
 let template = require("../templates/taskView/taskViewTpl.hbs");
 
-let TaskView = Backbone.View.extend({
+export let TaskView = Backbone.View.extend({
   initialize: function(options) {
     this.taskData = options.taskData;
   },
 
   render: function() {
-    this.$el.html(template(taskData));
+    this.$el.html(template(this.taskData));
     return this;
   }
 });
-
-let taskData;
-taskData = {
-  number: 1,
-  taskName : "Some name",
-  taskDescription: "Some description",
-  taskSolution: ""
-};
-
-export let taskView = new TaskView({taskData:taskData});
 
