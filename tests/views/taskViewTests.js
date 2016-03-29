57f315cb99b4f35ed274fe167100afa3d1e45611
Dmytro Fedorenko <you@example.com>
2016-03-29 12:58:26 +0300

    User can see result of solution verification.Tests

--- tests/views/taskViewTests.js
+++ tests/views/taskViewTests.js
@@ -8,33 +8,32 @@ import {TaskView} from '../../app/views/TaskView.js';
 
 describe('Task View Tests', function () {
 
-  let taskView;
+  let taskView,
+      taskModel,
+      taskData;
 
   beforeEach(()=>{
-    taskView = new TaskView();
+    taskModel = new Backbone.Model(taskData);
+    taskData = {"some": "data"};
+    taskView = new TaskView({model:taskModel});
   });
 
   describe("method prepareData", ()=>{
     it('should convert task data to JSON when method is called', ()=>{
-      let taskData = {"some": "data"};
-      taskView.model = new Backbone.Model(taskData);
+      taskModel = new Backbone.Model(taskData);
       taskView.prepareData();
       expect(taskView.taskData).to.eql(taskData);
     });
-
   });
 
   it("should render when model trigger event 'change'", ()=>{
-    let taskData = {"some": "data"};
     sinon.spy(taskView,"render");
-    taskView.model.set(taskData);
+    taskModel.set(taskData);
 
     expect(taskView.render.called).to.equal(true);
   });
 
   it("should trigger event 'checkSolution' when button clicked", ()=>{
-    let taskData = {"some": "data"};
-    taskView.model = new Backbone.Model(taskData);
     taskView.prepareData().render();
     sinon.spy(taskView,"trigger");
 
@@ -44,13 +43,47 @@ describe('Task View Tests', function () {
   });
 
   it("should send model.attributes when event 'checkSolution' triggered", ()=>{
-    let taskData = {"some": "data"};
-    taskView.model = new Backbone.Model(taskData);
     taskView.prepareData().render();
     sinon.spy(taskView,"trigger");
 
     taskView.$("button").click();
 
-    expect(taskView.trigger.calledWith("checkSolution", taskView.model.toJSON())).to.equal(true);
+    expect(taskView.trigger.calledWith("checkSolution", taskModel)).to.equal(true);
+  });
+
+  it("should prevent call method 'prepareData' and 'render' when model set call with options = {stop, true}", ()=>{
+    taskView.prepareData().render();
+    sinon.spy(taskView, "prepareData");
+    sinon.spy(taskView, "render");
+
+    taskModel.set("some2", "data2", {stop:true});
+
+    expect(taskView.prepareData.called).to.equal(false);
+    expect(taskView.render.called).to.equal(false);
+  });
+
+  describe("notifications behaviour", ()=>{
+    it("should render with hide any notifications", ()=>{
+      taskView.prepareData().render();
+
+      expect(taskView.$(".notification").width() == 0).to.equal(true);
+      expect(taskView.$(".notification").height() == 0).to.equal(true);
+    });
+
+    it("should show success notification when model attribute 'isSolved' is changed", ()=>{
+      taskView.prepareData().render();
+      let isSolvedAttr = {"isSolved": true};
+      taskModel.set(isSolvedAttr, {stop:true});
+
+      expect(taskView.$(".success").attr("display") == "none").to.not.equal(true);
+    });
+
+    it("should show error notification when model attribute 'isSolved' is changed", ()=>{
+      taskView.prepareData().render();
+      let isSolvedAttr = {"isSolved": false};
+      taskModel.set(isSolvedAttr, {stop:true});
+
+      expect(taskView.$(".error").attr("display") == "none").to.not.equal(true);
+    });
   });
 });
