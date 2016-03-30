67102621d5edebbc39383bae2c466cfd83c531c4
Dmytro Fedorenko <you@example.com>
2016-03-30 15:39:07 +0300

    User can navigate to next problem. Test#2

--- tests/views/testViewTests.js
+++ tests/views/testViewTests.js
@@ -10,7 +10,13 @@ describe('Test View Tests', function () {
   beforeEach(()=>{
     testModel = new Backbone.Model();
     testView = new TestView({model: testModel});
+    document.body.appendChild(testView.el);
   });
+
+  afterEach(()=>{
+    testView.$el.remove();
+  });
+
 
   it("should get data when method 'getTestData' is called", ()=>{
     sinon.stub(testModel, "trigger");
@@ -34,5 +40,42 @@ describe('Test View Tests', function () {
     expect(testView.$el.children()).to.have.length.above(1);
   });
 
+  //it("should check solution and set receive data to ", ()=>{
+  //
+  //});
+
+  describe("navigation behaviour", ()=>{
+    it("should change view to next when button 'prev' is pressed", ()=>{
+      let startView;
+      let currentView;
+      testView.render();
+      testView.getTestData();
+      testView.taskViews.forEach(function(elem){
+        elem.hide();
+      });
+
+      testView.taskViews[1].show();
+      testView.$("button.prev").click();
+      expect(testView.taskViews[1].isShow()).to.equal(false);
+      expect(testView.taskViews[0].isShow()).to.equal(true);
+
+    });
+
+    it("should change view to next when button 'next' is pressed", ()=>{
+      let startView;
+      let currentView;
+      testView.render();
+      testView.getTestData();
+      testView.taskViews.forEach(function(elem){
+        elem.hide();
+      });
+
+      testView.taskViews[0].show();
+      testView.$("button.next").click();
+      expect(testView.taskViews[0].isShow()).to.equal(false);
+      expect(testView.taskViews[1].isShow()).to.equal(true);
+
+    });
+  });
 });
 
